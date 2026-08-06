7cc6d6042063f3b06954818690f18fbbefa04238
Augis <augustas.stankevicius@tuta.io>
2026-08-06 23:15:31 +0300

    Add IS format to spec

--- spec.h
+++ spec.h
@@ -85,6 +85,7 @@ typedef enum {
 #define POS_R2 18
 #define POS_IMM14 18
 #define POS_IMM19 13
+#define POS_SHAMT 18
 
 #define MASK_TAG 0xF // 4 bits
 #define MASK_FN 0xF  // 4 bits
@@ -93,5 +94,6 @@ typedef enum {
 #define MASK_REG 0x1F      // 5 bits
 #define MASK_IMM14 0x3FFF  // 14 bits
 #define MASK_IMM19 0x7FFFF // 19 bits
+#define MASK_SHAMT 0x1F    // 5 bits
 
 #endif // SPEC_H
